c31eb310776576916953a768897204e1cf6d1155
Gollay <110508890+GollayDPengfei@users.noreply.github.com>
2022-08-07 18:51:15 -0700

    Add files via upload

---
+++ Phyton1.py
@@ -0,0 +1,12 @@
+# Belajar Output
+
+print("Hello World")
+print("Nama Saya Gollay Pengfei")
+print("Saya Menyukai Anjing+Kucing")
+print("Hobi Bermain Ping Pong")
+print("Makanan Kesukaan spaghetti")
+print("Membaca Komik/Light Novel")
+print("Idola Kesukaan Jihyo TWICE")
+print("Higher-ups? Just a Bunch Of Hypocrite")
+print("WhenTheyCry")
+print("Favorite Lagu JELEEL!-DIVE IN!")
---
+++ python2 .py
@@ -0,0 +1,13 @@
+#Belajar InPut
+
+Nama = "Gollay" # Nama Adalah Data, Gollay Adalah Variabel
+Umur = 15 # Tipe Data Integer
+Alamat ="Cimanggis, Kota Depok"
+
+print(Nama)
+print(Umur)
+print(Alamat)
+
+print("Nama saya adalah " + Nama)
+
+# print(" nama saya adalah " + nama + " Umur Saya ") + str (umur) + " alamat saya " + alamat)

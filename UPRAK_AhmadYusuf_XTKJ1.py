32fe4a3d5a37ce2ea042f5bc9b0b79fd1b8f1089
Gollay <110508890+GollayDPengfei@users.noreply.github.com>
2022-08-11 20:44:40 -0700

    Add files via upload

---
+++ UPRAK_AhmadYusuf_XTKJ1.py
@@ -0,0 +1,10 @@
+
+
+namadepan = input("Masukkan nama anda: ")
+namabelakang = input("Masukkan nama belakang anda: ")
+
+print("nama anda adalah", namadepan, namabelakang)
+
+suhu = int(input("Masukkan suhu celcius: "))
+reamur = (4/5 * suhu)
+print(suhu, "celcius adalah", reamur, "reamur")
